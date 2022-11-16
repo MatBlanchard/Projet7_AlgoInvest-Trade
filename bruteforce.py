1e40e0b20409cc7ddbd0a93fd7434557050af06f
Mathieu Blanchard <matblanchard.pro@gmail.com>
2022-11-16 16:47:34 +0100

    algorithme optimisé fonctionnel

--- bruteforce.py
+++ bruteforce.py
@@ -10,25 +10,24 @@ def get_combination(max_value, elements, elements_selection=None):
     if elements_selection is None:
         elements_selection = []
     if elements:
-        val1, cost, lst_val1 = get_combination(max_value, elements[1:], elements_selection)
+        val1, lst_val1 = get_combination(max_value, elements[1:], elements_selection)
         val = elements[0]
         if val[1] <= max_value:
-            val2, cost, lst_val2 = get_combination(max_value - val[1], elements[1:], elements_selection + [val])
+            val2, lst_val2 = get_combination(max_value - val[1], elements[1:], elements_selection + [val])
             if val1 < val2:
-                return val2, cost, lst_val2
-        return val1, cost, lst_val1
+                return val2, lst_val2
+        return val1, lst_val1
     else:
-        cost = sum([i[1] for i in elements_selection])
-        return sum([i[2] for i in elements_selection]) - cost, cost, elements_selection
+        return sum([i[2] for i in elements_selection]), elements_selection
 
 
 with open("data/dataset_1.csv") as file:
     reader = file.readlines()[1:]
     for row in reader:
         temp = row.split(",")
-        datas.append((temp[0], float(temp[1]), float(temp[1])*(1+float(temp[2])/100)))
-profit, total_cost, actions = get_combination(500, datas)
+        datas.append((temp[0], float(temp[1]), float(temp[1])*(1+float(temp[2])/100)-float(temp[1])))
+profit, actions = get_combination(500, datas)
 print("Actions: " + str([action[0] for action in actions]))
-print('Total cost = {:.2f}€'.format(total_cost))
+print('Total cost = {:.2f}€'.format(sum([action[1] for action in actions])))
 print('Total profit = {:.2f}€'.format(profit))
 print('Solution found in {:}ms'.format(round(1000*(time.time() - start))))
--- optimized.py
+++ optimized.py
@@ -1,20 +1,42 @@
 import time
+from math import floor, ceil
 
 start = time.time()
 datas = []
 
 
 def get_combination(max_value, elements):
-    return 0, 0, []
+    matrice = [[0 for x in range(max_value + 1)] for x in range(len(elements) + 1)]
+    for i in range(1, len(elements) + 1):
+        for w in range(1, max_value + 1):
+            if elements[i-1][1] <= w:
+                matrice[i][w] = max(elements[i-1][2] + matrice[i-1][w-elements[i-1][1]], matrice[i-1][w])
+            else:
+                matrice[i][w] = matrice[i-1][w]
+
+    w = max_value
+    n = len(elements)
+    elements_selection = []
+    while w >= 0 and n >= 0:
+        e = elements[n-1]
+        if matrice[n][w] == matrice[n-1][w-e[1]] + e[2]:
+            elements_selection.append(e)
+            w -= e[1]
+        n -= 1
+    return matrice[-1][-1], elements_selection
 
 
-with open("data/dataset_1.csv") as file:
+with open("data/dataset_3.csv") as file:
     reader = file.readlines()[1:]
     for row in reader:
         temp = row.split(",")
-        datas.append((temp[0], float(temp[1]), float(temp[1])*(1+float(temp[2])/100)))
-profit, total_cost, actions = get_combination(500, datas)
+        cost = round(float(temp[1])*100)
+        if cost < 0:
+            cost = 0
+        profit = cost*(1+float(temp[2])/100)-cost
+        datas.append((temp[0], cost, profit))
+profit, actions = get_combination(50000, datas)
 print("Actions: " + str([action[0] for action in actions]))
-print('Total cost = {:.2f}€'.format(total_cost))
-print('Total profit = {:.2f}€'.format(profit))
+print('Total cost = {:.2f}€'.format(sum([action[1]/100 for action in actions])))
+print('Total profit = {:.2f}€'.format(profit/100))
 print('Solution found in {:}ms'.format(round(1000*(time.time() - start))))
